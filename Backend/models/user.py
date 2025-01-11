581a17d89d330b730383abcc9f51d8aa8cfb1f0b
Kristy <kprieto14@gmail.com>
2025-01-11 14:34:09 -0500

    Add schemas for upcoming request

--- Backend/models/user.py
+++ Backend/models/user.py
@@ -4,9 +4,9 @@ class UsersModel(db.Model):
     __tablename__ = 'Users'
 
     Id = db.Column(db.Integer, primary_key=True)
-    Email = db.Column(db.String, nullable=False)
+    Email = db.Column(db.String, nullable=False, unique=True)
     HashedPassword = db.Column(db.String, nullable=True)
-    Username = db.Column(db.String, nullable=False)
+    Username = db.Column(db.String, nullable=False, unique=True)
 
     # Relationships
     birds = db.relationship('BirdsModel', back_populates='user', lazy='dynamic', cascade='all, delete')
---
+++ Backend/schemas/birdOfTheDayScm.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from marshmallow import Schema, fields, validate, validates_schema
+
+seasons = [ 'Spring', 'Summer', 'Fall', 'Winter' ]
+
+class PlainBirdSchema(Schema):
+    Id = fields.Integer(dump_only=True)
+    Name = fields.String(required = True)
+    AdoptedFrom = fields.String(required=False)
+    YearPublished = fields.Integer(
+                                    required=True,
+                                    validate=validate.Length(min=4, max=4),
+                                    validate=validate.Range(min=2022, max=datetime.now().year)
+                                )
+    SeasonCollection = fields.String(required=True, validate=validate.OneOf(seasons))
+    HolidayCollection = fields.String(required=False)
+    PhotoUrl = fields.String(required=False)
+    PhotoPublicId = fields.String(required=False)
+    PhotoFileName = fields.String(required=False)
+    UserId = fields.Integer(dump_only=True)
+
+class PlainBirdOfTheDaySchema(Schema):
+    Id = fields.Integer(dump_only=True)
+    UserName = fields.String(required = False)
+    ChosenDate = fields.DateTime(required=False)
+    BirdId = fields.Integer(dump_only=True)
+
+class BirdOfTheDaySchema(PlainBirdOfTheDaySchema):
+    BirdOfTheDay = fields.Nested(PlainBirdSchema, dump_only=True)
